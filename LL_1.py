28f083267f4efe3c9e22f40d869785bf7270c8f0
ESneiders <edvards.sneiders@edu.rtu.lv>
2025-02-11 09:45:40 +0200

    LL_1.py

--- LL_1.py
+++ LL_1.py
@@ -1,15 +1,13 @@
 class Node:
     def __init__(self, value):
         self.value = value
-        self.next = None
-        
+        self.next = None        
 
 class LinkedList:
     def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
-
         
     def append(self, value):
         new_node = Node(value)
@@ -21,15 +19,15 @@ class LinkedList:
             self.tail = new_node
         return True
         
-
-    # WRITE FIND_MIDDLE_NODE METHOD HERE #
-    #                                    #
-    #                                    #
-    #                                    #
-    #                                    #
-    ######################################
-
-
+    def find_middle_node(self):
+        slow_node = self.head
+        fast_node = self.head
+        
+        while fast_node is not None and fast_node.next is not None:
+            slow_node = slow_node.next
+            fast_node = fast_node.next.next
+            
+        return slow_node
 
 my_linked_list = LinkedList(1)
 my_linked_list.append(2)
@@ -37,5 +35,5 @@ my_linked_list.append(3)
 my_linked_list.append(4)
 my_linked_list.append(5)
 
-print( my_linked_list.find_middle_node().value )
+print(my_linked_list.find_middle_node().value)
 
